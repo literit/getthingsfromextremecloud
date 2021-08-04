73bf494218d3a3a9a2f92e92fb2bf064375cb5f6
literit <62394144+literit@users.noreply.github.com>
2021-08-04 13:25:52 -0700

    Nows prints list of client data via tabulate also added readme

--- api.py
+++ api.py
@@ -1,26 +1,21 @@
 import requests
 import json
 from pprint import pprint as pp
+from tabulate import tabulate
 from secret import *
 #the variable pwd requires a seperate file named secret.py with the variable pwd = {'username': 'insert username here', 'password': 'insert password here'} with the two fields being filed in
 
 login = requests.post("https://api.extremecloudiq.com/login", json=pwd)
-#print(login.text)
 login_text = json.loads(login.text)
-#print(login_text)
 authkey = {}
 authkey["Authorization"] = "Bearer " + login_text["access_token"]
-#print(authkey["Authorization"])
-#print(type(authkey))
-clients = requests.get("https://api.extremecloudiq.com/clients/active", headers = authkey)
-#pp(json.loads(clients.text))
-pp(clients.json())
-print("\n\n\n")
 devices = requests.get("https://api.extremecloudiq.com/devices?page=1&limit=100", headers = authkey)
-pp(devices.json())
-print("\n\n\n")
-network_policies = requests.get("https://api.extremecloudiq.com/network-policies?page=1&limit=100", headers = authkey)
-pp(network_policies.json())
-print("\n\n\n")
-ssids = requests.get("https://api.extremecloudiq.com/ssids?page=1&limit=100", headers = authkey)
-pp(ssids.json())
+devlist = devices.json()
+id2device = {}
+for device in devlist['data']:
+    id2device[device['id']] = device
+clients = requests.get("https://api.extremecloudiq.com/clients/active", headers = authkey)
+clientlist = []
+for client in clients.json()['data']:
+    clientlist.append([client['id'], client['hostname'], client['ip_address'], client['mac_address'], client['ssid'], client['device_id'], id2device[client['device_id']]['hostname'], id2device[client['device_id']]['ip_address']])
+print(tabulate(clientlist, headers=['CL_ID', 'CL_Name', 'CL_IP', 'CL_MAC', 'CL_SSID', 'Dev_ID', 'Dev_Name', 'Dev_IP']))
---
+++ readme.md
@@ -0,0 +1,3 @@
+## GetThingsFromExtremeCloud
+
+This python script gets you a list of information about clients, such as their ID, what device they are connected to, et cetera.
