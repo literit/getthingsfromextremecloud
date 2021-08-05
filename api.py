7e6c411e94043d6e08524abf3ba0a286503aa98f
literit <62394144+literit@users.noreply.github.com>
2021-08-04 17:41:39 -0700

    Added comments

--- api.py
+++ api.py
@@ -5,16 +5,26 @@ from tabulate import tabulate
 from secret import *
 #the variable pwd requires a seperate file named secret.py with the variable pwd = {'username': 'insert username here', 'password': 'insert password here'} with the two fields being filed in
 
+#Login to api
 login = requests.post("https://api.extremecloudiq.com/login", json=pwd)
-login_text = json.loads(login.text)
+
+#Create authentication key from the given access token
+login_text = login.json()
 authkey = {}
 authkey["Authorization"] = "Bearer " + login_text["access_token"]
+
+#Gets list of devices and their data
 devices = requests.get("https://api.extremecloudiq.com/devices?page=1&limit=100", headers = authkey)
-devlist = devices.json()
+
+#Creates a translation table for device IDs to actual Device
 id2device = {}
-for device in devlist['data']:
+for device in devices.json()['data']:
     id2device[device['id']] = device
+
+#Gets list of clients and their data
 clients = requests.get("https://api.extremecloudiq.com/clients/active", headers = authkey)
+
+#creates a list of client and device data
 clientlist = []
 for client in clients.json()['data']:
     clientlist.append([client['id'], client['hostname'], client['ip_address'], client['mac_address'], client['ssid'], client['device_id'], id2device[client['device_id']]['hostname'], id2device[client['device_id']]['ip_address']])
